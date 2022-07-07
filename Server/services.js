c31f5520c5e57d908d9839ad9c9b92a048270d33
clarababette <clara_coetzee@zoho.com>
2022-07-07 16:03:51 +0200

    enabled middleware

--- Server/services.js
+++ Server/services.js
@@ -78,12 +78,14 @@ function Services(db) {
     const authHeader = req.headers['authorization'];
     if (!authHeader) return res.sendStatus(401);
     const token = authHeader.split(' ')[1];
-    console.log(req.headers)
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) return res.send(err);
-      req.user = decoded.username;
-      next();
-    });
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
+
   };
 
   const handleRefreshToken = (req, res) => {
